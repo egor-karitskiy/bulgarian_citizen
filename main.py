6a4dfb4f1a20462f519dfbc567f33b67aca88409
ekaritskiy <egor.karitskiy@gmail.com>
2022-11-19 19:00:49 +0300

    persistence DB routine update

--- main.py
+++ main.py
@@ -7,30 +7,19 @@ import requests
 import logging
 import datetime
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
-from PGpersistence import PostgresPersistence
-
-from telegram import __version__ as TG_VER
-
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)
-
-if __version_info__ < (20, 0, 0, "alpha", 1):
-    raise RuntimeError(
-        f"This bot is not compatible with your current version {TG_VER}."
-    )
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, Bot
 from telegram.ext import (
     Application,
     CommandHandler,
     ContextTypes,
     ConversationHandler,
     MessageHandler,
-    filters, PicklePersistence,
+    filters,
 )
 
 load_dotenv()
@@ -53,7 +42,6 @@ reply_keyboard = [
 ]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
@@ -98,30 +86,28 @@ def request_status(req_num, pin):
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Start the conversation, display any stored data and ask user for input."""
-    reply_text = f"Hi! My name is Bulgarian Citizen bot — to be shorty — BulCit " \
-                 f"(nothing familiar to 'Bullshit'!). \n" \
-                 f"I help check and monitor statuses of your Bulgarian " \
-                 f"citizenship petition! \n"
+    is_new_user = True
+    user_id = update.message.from_user.id
+    if user_petition_number_from_db(user_id) is not None or user_pin_from_db(user_id) is not None:
+        is_new_user = False
 
-    if 'pin' not in context.user_data or 'petition number' not in context.user_data:
-        reply_text += (
-            "Please provide credentials (petition number and PIN) given by "
-            "Bulgarian Ministry of Justice. \n"
-            "Push corresponding buttons below to provide info to me."
+    if is_new_user:
+        reply_text = f"Hi! My name is Bulgarian Citizen bot — to be shorty — BulCit " \
+                     f"(nothing familiar to 'Bullshit'!). \n" \
+                     f"I help check and monitor statuses of your Bulgarian " \
+                     f"citizenship petition! \n" \
+                     f"Please provide credentials (petition number and PIN) given by " \
+                     f"Bulgarian Ministry of Justice. \n" \
+                     f"Push corresponding buttons below to provide info to me."
 
-        )
     else:
-        reply_text += (
-            "You have already provided your PIN and petition number to me. \n"
-            "If you like to change credentials please use corresponding buttons below."
-        )
+        reply_text = f"Hi again! Here I am! The BulCit bot! \n" \
+                     f"If you'd like to update your credentials (PIN or petition number)" \
+                     f" please use buttons below. \n" \
+                     f"If you'd like to see freshly updated status of your petition " \
+                     f"please push 'Done' button. \n"
 
     await update.message.reply_text(reply_text, reply_markup=markup)
-
-    current_jobs = context.job_queue.jobs()
-    for job in current_jobs:
-        job.schedule_removal()
 
     return CHOOSING
 
@@ -129,14 +115,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
     text = update.message.text.lower()
-    context.user_data["choice"] = text
-    if context.user_data.get(text):
-        reply_text = (
-            f"Please send me your {text}. Saved {text} is: {context.user_data[text]}"
-        )
-    else:
-        reply_text = f"Please send me your {text}."
-    await update.message.reply_text(reply_text)
+    context.user_data["button"] = text
+    user_id = update.message.from_user.id
+    if user_pin_from_db(user_id) is None or user_petition_number_from_db(user_id) is None:
+        new_user_creds_record(user_id)
+    await update.message.reply_text(f"Please send me your {text}, or type NO if you don't like to "
+                                    f"change {text} already provided by you earilier.")
 
     return TYPING_REPLY
 
@@ -144,82 +128,91 @@ async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Store info provided by user and ask for the next category."""
     text = update.message.text
-    category = context.user_data["choice"]
-    context.user_data[category] = text.lower()
-    del context.user_data["choice"]
+    pressed_button = context.user_data["button"]
+    del context.user_data["button"]
 
-    await update.message.reply_text(
-        f"Current info provided by you is the following:\n"
-        f"%s\n"
-        f"%s\n"
+    user_id = update.message.from_user.id
 
-        % (
-            f"Your petition number: {context.user_data['petition number']}"
-            if 'petition number' in context.user_data
-            else "Petition number is not provided yet",
+    is_wrong_input = False
+    if text.lower() == 'no' or text.lower() == 'pin' or text.lower() == 'petition number':
+        is_wrong_input = True
 
-            f"Your PIN: {context.user_data['pin']}"
-            if 'pin' in context.user_data
-            else "PIN is not provided yet"),
-        reply_markup=markup,
-    )
+    is_pin_provided = True
+    if user_pin_from_db(user_id) is None or user_pin_from_db(user_id) == '0':
+        is_pin_provided = False
+
+    is_petition_number_provided = True
+    if user_petition_number_from_db(user_id) is None or user_petition_number_from_db(user_id) == '0':
+        is_petition_number_provided = False
+
+    if pressed_button == 'pin' and not is_wrong_input:
+        update_user_pin(user_id, text)
+        is_pin_provided = True
+    if pressed_button == 'petition number' and not is_wrong_input:
+        update_user_petition_number(user_id, text)
+        is_petition_number_provided = True
+
+    if is_pin_provided and is_petition_number_provided:
+        reply_text = f"Current info provided by you is the following:\n" \
+                     f"Petition number: {user_petition_number_from_db(user_id)} \n" \
+                     f"PIN: {user_pin_from_db(user_id)}\n" \
+                     f"We are all set! Please push 'Done' button."
+    elif is_pin_provided and not is_petition_number_provided:
+        reply_text = f"PIN is provided. But petition number is not!\n" \
+                     f"Please push 'Petition number' button!: {user_petition_number_from_db(user_id)}, {is_petition_number_provided}"
+    elif not is_pin_provided and is_petition_number_provided:
+        reply_text = f"Petition number is provided. But PIN is not!\n" \
+                     f"Please push 'PIN' button!:"
+    else:
+        is_petition_number_provided = True
+        reply_text = f"Credentials are not provided yet.\n" \
+                     f"Please push 'Petition number' or 'PIN' button!: {user_petition_number_from_db(user_id)}, {is_petition_number_provided}"
+
+    await update.message.reply_text(reply_text, reply_markup=markup)
+    print(is_pin_provided, is_petition_number_provided)
 
     return CHOOSING
 
 
 async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user_id = update.message.from_user.id
+
+    is_pin_provided = True
+    if user_pin_from_db(user_id) is None or user_pin_from_db(user_id) == '0':
+        is_pin_provided = False
+
+    is_petition_number_provided = True
+    if user_petition_number_from_db(user_id) is None or user_petition_number_from_db(user_id) == '0':
+        is_petition_number_provided = False
+
     await update.message.reply_text(
         f"Current info provided by you is the following:\n"
         f"%s\n"
         f"%s\n"
         f"\n"
         f"%s\n"
-        # f"{context.user_data['status']}"
-        % (
-            f"Your petition number: {context.user_data['petition number']}"
-            if 'petition number' in context.user_data
-            else "Petition number is not provided yet",
 
-            f"Your PIN: {context.user_data['pin']}"
-            if 'pin' in context.user_data
-            else "PIN is not provided yet",
+        % (
+            f"Your petition number: {user_petition_number_from_db(user_id)}"
+            if is_petition_number_provided
+            else "Petition number is not provided yet. Please use /start menu.",
+
+            f"Your PIN: {user_pin_from_db(user_id)}"
+            if is_pin_provided
+            else "PIN is not provided yet. Please use /start menu.",
 
             f"Status of your petition is:\n"
-            f"{request_status(context.user_data['petition number'], context.user_data['pin'])}"
-            if 'petition number' and 'pin' in context.user_data
-            else "No sufficient data for petition status request"
+            f"{request_status(user_petition_number_from_db(user_id), user_pin_from_db(user_id))}\n"
+            f"\n"
+            f"Monitoring is ON.\n"
+            f"I'll let you know when status is changed."
+            if is_pin_provided and is_petition_number_provided
+            else ""
         ),
 
         reply_markup=ReplyKeyboardRemove(),
     )
-
-    job_queue = context.job_queue
-    if 'pin' in context.user_data and 'petition number' in context.user_data:
-        job_queue.run_repeating(check_status_routine,
-                                interval=3600,
-                                chat_id=update.effective_user.id,
-                                data=context.user_data)
-        await update.message.reply_text('Status monitoring is on', reply_markup=ReplyKeyboardRemove(), )
     return ConversationHandler.END
-
-
-async def check_status_routine(context: ContextTypes.DEFAULT_TYPE) -> None:
-    pin = context.job.data['pin']
-    num = context.job.data['petition number']
-    user_id = context.job.data['id']
-    result = request_status(num, pin)
-
-    if last_status(user_id) is not None:
-        if last_status(user_id) != result:
-            append_new_status(user_id, result)
-            status_changed_message = f"Petition status has been changed!\n" \
-                                     f"New status is: {result}"
-            await context.bot.send_message(context.job.chat_id, text=status_changed_message)
-    else:
-        append_new_status(user_id, result)
-        first_time_status_message = f"Petition status is the following:\n" \
-                                    f"{result}"
-        await context.bot.send_message(context.job.chat_id, text=first_time_status_message)
 
 
 def append_new_status(user_id, status_text):
@@ -266,9 +259,160 @@ def last_status(user_id):
         )
 
 
+def user_petition_number_from_db(user_id):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_select_query = f"SELECT * FROM creds WHERE user_id = '{user_id}'"
+        cursor.execute(sql_select_query)
+        select_result = cursor.fetchall()
+        if not select_result:
+            return None
+        else:
+            return select_result[0][1]
+
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            f"DB error {error}."
+        )
+
+
+def user_pin_from_db(user_id):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_select_query = f"SELECT * FROM creds WHERE user_id = '{user_id}'"
+        cursor.execute(sql_select_query)
+        select_result = cursor.fetchall()
+        if not select_result:
+            return None
+        else:
+            return select_result[0][2]
+
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            f"DB error {error}."
+        )
+
+
+def new_user_creds_record(user_id):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO creds (user_id, petition_no, pin) VALUES (%s,%s,%s)"""
+        record_to_insert = (user_id, '0', '0')
+        cursor.execute(sql_insert_query, record_to_insert)
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            f"DB error {error}."
+        )
+
+
+def update_user_pin(user_id, pin):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_insert_query = f"UPDATE creds SET pin='{pin}' where user_id='{user_id}'"
+        cursor.execute(sql_insert_query)
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            f"DB error {error}."
+        )
+
+
+def update_user_petition_number(user_id, petition_no):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_insert_query = f"UPDATE creds SET petition_no='{petition_no}' where user_id='{user_id}'"
+        cursor.execute(sql_insert_query)
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            f"DB error {error}."
+        )
+
+
+def get_users_ids_from_db():
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_select_query = f"SELECT user_id FROM creds"
+        cursor.execute(sql_select_query)
+        select_result = cursor.fetchall()
+        if not select_result:
+            return None
+        else:
+            return select_result
+
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            f"DB error {error}."
+        )
+
+
+async def checking_statuses_routine():
+    bot = Bot(TELEGRAM_TOKEN)
+    users_list = get_users_ids_from_db()
+    for user_record in users_list:
+        for user_id in user_record:
+            fresh_status = request_status(user_petition_number_from_db(user_id), user_pin_from_db(user_id))
+            last_status_from_db = last_status(user_id)
+            if fresh_status != last_status_from_db:
+                try:
+                    await bot.send_message(user_id, f'Hey there!!! Here is the new status of your '
+                                                    f'petition: \n '
+                                                    f'{fresh_status}!')
+                except Exception as error:
+                    raise RuntimeError(f'Message sent error: {error}')
+                append_new_status(user_id, fresh_status)
+
+
 def main() -> None:
     """Run the bot."""
-    application = Application.builder().token(TELEGRAM_TOKEN).persistence(PostgresPersistence(url=DB_URI)).build()
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(checking_statuses_routine, 'interval', seconds=10800)
+    scheduler.start()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
@@ -291,7 +435,7 @@ def main() -> None:
         },
         fallbacks=[MessageHandler(filters.Regex("^Done$"), done)],
         name="my_conversation",
-        persistent=True,
+        persistent=False,
     )
 
     application.add_handler(conv_handler)
