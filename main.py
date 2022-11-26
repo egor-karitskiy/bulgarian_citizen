9a1aff333d2abc5bbdc880663d95975e9186a9f2
ekaritskiy <egor.karitskiy@gmail.com>
2022-11-26 14:35:55 +0300

    multilanguage support

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import psycopg2
 import requests
 import logging
 import datetime
+import gettext
 
 from tabulate import tabulate
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -48,6 +49,11 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+t_en = gettext.translation("main", localedir="locale", languages=["en"], fallback=True)
+t_ru = gettext.translation("main", localedir="locale", languages=["ru"], fallback=True)
+
+t_en.install()
+
 
 def get_req_token():
     r = requests.get("https://publicbg.mjs.bg/BgInfo/Home/Enroll")
@@ -80,33 +86,26 @@ def request_status(req_num, pin):
     if status_object:
         status = status_object.group(1)
         if 'Липсват данни' in status:
-            return "Incorrect credentials"
+            return _("Incorrect credentials")
     else:
-        status = "No status appeared"
+        status = (_("No status appeared"))
     return status
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     is_new_user = True
     user_id = update.message.from_user.id
+    language_code = update.message.from_user.language_code
+    if language_code != 'ru':
+        language_code = 'en'
     if user_petition_number_from_db(user_id) is not None or user_pin_from_db(user_id) is not None:
         is_new_user = False
 
     if is_new_user:
-        reply_text = f"Hi! My name is Bulgarian Citizen bot — to be shorty — BulCit " \
-                     f"(nothing familiar to 'Bullshit'!). \n" \
-                     f"I help check and monitor statuses of your Bulgarian " \
-                     f"citizenship petition! \n" \
-                     f"Please provide credentials (petition number and PIN) given by " \
-                     f"Bulgarian Ministry of Justice. \n" \
-                     f"Push corresponding buttons below to provide info to me."
+        reply_text = get_translated_message('new_user_welcome_message', language_code)
 
     else:
-        reply_text = f"Hi again! Here I am! The BulCit bot! \n" \
-                     f"If you'd like to update your credentials (PIN or petition number)" \
-                     f" please use buttons below. \n" \
-                     f"If you'd like to see freshly updated status of your petition " \
-                     f"please push 'Done' button. \n"
+        reply_text = get_translated_message('existing_user_welcome_message', language_code)
 
     await update.message.reply_text(reply_text, reply_markup=markup)
 
@@ -118,10 +117,19 @@ async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     text = update.message.text.lower()
     context.user_data["button"] = text
     user_id = update.message.from_user.id
+    language_code = update.message.from_user.language_code
+    if language_code != 'ru':
+        language_code = 'en'
     if user_pin_from_db(user_id) is None or user_petition_number_from_db(user_id) is None:
-        new_user_creds_record(user_id)
-    await update.message.reply_text(f"Please send me your {text}, or type NO if you don't like to "
-                                    f"change {text} already provided by you earlier.")
+        new_user_creds_record(user_id, language_code)
+
+    if text == 'pin':
+        reply_text = get_translated_message('give_me_your_pin', language_code)
+    elif text == 'petition number':
+        reply_text = get_translated_message('give_me_your_petition_number', language_code)
+    else:
+        reply_text = ''
+    await update.message.reply_text(reply_text)
 
     return TYPING_REPLY
 
@@ -131,6 +139,10 @@ async def received_information(update: Update, context: ContextTypes.DEFAULT_TYP
     text = update.message.text
     pressed_button = context.user_data["button"]
     del context.user_data["button"]
+
+    language_code = update.message.from_user.language_code
+    if language_code != 'ru':
+        language_code = 'en'
 
     user_id = update.message.from_user.id
 
@@ -154,19 +166,14 @@ async def received_information(update: Update, context: ContextTypes.DEFAULT_TYP
         is_petition_number_provided = True
 
     if is_pin_provided and is_petition_number_provided:
-        reply_text = f"Current info provided by you is the following:\n" \
-                     f"Petition number: {user_petition_number_from_db(user_id)} \n" \
-                     f"PIN: {user_pin_from_db(user_id)}\n" \
-                     f"We are all set! Please push 'Done' button."
+        reply_text = (get_translated_message('all_set_message', language_code)
+                      % (user_petition_number_from_db(user_id), user_pin_from_db(user_id)))
     elif is_pin_provided and not is_petition_number_provided:
-        reply_text = f"PIN is provided. But petition number is not!\n" \
-                     f"Please push 'Petition number' button!: {user_petition_number_from_db(user_id)}, {is_petition_number_provided}"
+        reply_text = get_translated_message('pin_provided_pn_not', language_code)
     elif not is_pin_provided and is_petition_number_provided:
-        reply_text = f"Petition number is provided. But PIN is not!\n" \
-                     f"Please push 'PIN' button!:"
+        reply_text = get_translated_message('pn_provided_pin_not', language_code)
     else:
-        reply_text = f"Credentials are not provided yet.\n" \
-                     f"Please push 'Petition number' or 'PIN' button!: {user_petition_number_from_db(user_id)}, {is_petition_number_provided}"
+        reply_text = get_translated_message('no_creds_provided', language_code)
 
     await update.message.reply_text(reply_text, reply_markup=markup)
 
@@ -175,6 +182,9 @@ async def received_information(update: Update, context: ContextTypes.DEFAULT_TYP
 
 async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_id = update.message.from_user.id
+    language_code = update.message.from_user.language_code
+    if language_code != 'ru':
+        language_code = 'en'
 
     is_pin_provided = True
     if user_pin_from_db(user_id) is None or user_pin_from_db(user_id) == '0':
@@ -193,41 +203,29 @@ async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     if is_pin_provided and is_petition_number_provided:
         user_statuses = tabulate(get_user_statuses_list(user_id), headers=['Status', 'Date'])
         days = datetime.datetime.now() - last_status_date(user_id)
-        days_str = time_delta_to_str(days, "{days} days")
+        days_str = time_delta_to_str(days, "{days}")
         log_record(user_id, fresh_status, 'user check')
     else:
         days_str = '0'
 
-    await update.message.reply_text(
-        f"Current info provided by you is the following:\n"
-        f"%s\n"
-        f"%s\n"
-        f"\n"
-        f"%s\n"
+    if is_petition_number_provided and is_pin_provided:
+        reply_text = (get_translated_message('done_full_message', language_code)
+                      % (user_petition_number_from_db(user_id),
+                         user_pin_from_db(user_id),
+                         fresh_status,
+                         days_str))
 
-        % (
-            f"Your petition number: {user_petition_number_from_db(user_id)}"
-            if is_petition_number_provided
-            else "Petition number is not provided yet. Please use /start menu.",
+    elif is_pin_provided and not is_petition_number_provided:
+        reply_text = (get_translated_message('pin_provided_pn_not', language_code)
+                      % (user_pin_from_db(user_id)))
 
-            f"Your PIN: {user_pin_from_db(user_id)}"
-            if is_pin_provided
-            else "PIN is not provided yet. Please use /start menu.",
+    elif is_petition_number_provided and not is_pin_provided:
+        reply_text = (get_translated_message('pn_provided_pin_not', language_code)
+                      % (user_petition_number_from_db(user_id)))
+    else:
+        reply_text = get_translated_message('done_no_creds', language_code)
 
-            f"Status of your petition is:\n"
-            f"{fresh_status}\n"
-            f"\n"
-            f"{days_str} passed since last status change.\n"
-            f"\n"
-            f"Monitoring is ON.\n"
-            f"I'll let you know when status is changed."
-
-            if is_pin_provided and is_petition_number_provided
-            else ""
-        ),
-
-        reply_markup=ReplyKeyboardRemove(),
-    )
+    await update.message.reply_text(reply_text, reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
 
@@ -261,7 +259,7 @@ def get_user_statuses_list(user_id):
 
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
@@ -275,7 +273,7 @@ def log_record(user_id, status_text, message):
             port=port
         )
         cursor = connection.cursor()
-        sql_insert_query = """ INSERT INTO logs (user_id, status_text, timestamp, message) VALUES (%s,%s,%s,%s)"""
+        sql_insert_query = f""" INSERT INTO logs (user_id, status_text, timestamp, message) VALUES (%s,%s,%s,%s)"""
 
         record_to_insert = (user_id, status_text, datetime.datetime.now(datetime.timezone.utc), message)
 
@@ -283,7 +281,7 @@ def log_record(user_id, status_text, message):
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
@@ -297,7 +295,7 @@ def append_new_status(user_id, status_text):
             port=port
         )
         cursor = connection.cursor()
-        sql_insert_query = """ INSERT INTO statuses (user_id, status, status_date) VALUES (%s,%s,%s)"""
+        sql_insert_query = f""" INSERT INTO statuses (user_id, status, status_date) VALUES (%s,%s,%s)"""
 
         if 'Образувана преписка' in status_text:
             year_date_text = re.search("(\d\d\.\d\d\.\d{4})", status_text)
@@ -315,8 +313,7 @@ def append_new_status(user_id, status_text):
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}.")
-
+            "DB error {error}.")
 
 
 def last_status(user_id):
@@ -339,7 +336,7 @@ def last_status(user_id):
 
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
@@ -363,7 +360,7 @@ def last_status_date(user_id):
 
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
@@ -387,7 +384,7 @@ def user_petition_number_from_db(user_id):
 
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
@@ -411,11 +408,11 @@ def user_pin_from_db(user_id):
 
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
-def new_user_creds_record(user_id):
+def new_user_creds_record(user_id, language):
     try:
         connection = psycopg2.connect(
             database=database,
@@ -425,13 +422,13 @@ def new_user_creds_record(user_id):
             port=port
         )
         cursor = connection.cursor()
-        sql_insert_query = """ INSERT INTO creds (user_id, petition_no, pin) VALUES (%s,%s,%s)"""
-        record_to_insert = (user_id, '0', '0')
+        sql_insert_query = f""" INSERT INTO creds (user_id, petition_no, pin, language) VALUES (%s,%s,%s,%s)"""
+        record_to_insert = (user_id, '0', '0', language)
         cursor.execute(sql_insert_query, record_to_insert)
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
@@ -450,7 +447,31 @@ def update_user_pin(user_id, pin):
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
+        )
+
+
+def get_translated_message(message_code, language):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_select_query = f"SELECT {language} FROM messages WHERE message_code = '{message_code}'"
+        cursor.execute(sql_select_query)
+        select_result = cursor.fetchall()
+        if not select_result:
+            return None
+        else:
+            return select_result[0][0]
+
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            "DB error {error}."
         )
 
 
@@ -469,7 +490,7 @@ def update_user_petition_number(user_id, petition_no):
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
         )
 
 
@@ -493,7 +514,31 @@ def get_users_ids_from_db():
 
     except (Exception, psycopg2.Error) as error:
         raise RuntimeError(
-            f"DB error {error}."
+            "DB error {error}."
+        )
+
+
+def user_language_from_db(user_id):
+    try:
+        connection = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        cursor = connection.cursor()
+        sql_select_query = f"SELECT * FROM creds WHERE user_id = '{user_id}'"
+        cursor.execute(sql_select_query)
+        select_result = cursor.fetchall()
+        if not select_result:
+            return None
+        else:
+            return select_result[0][3]
+
+    except (Exception, psycopg2.Error) as error:
+        raise RuntimeError(
+            "DB error {error}."
         )
 
 
@@ -504,14 +549,15 @@ async def checking_statuses_routine():
         for user_id in user_record:
             fresh_status = request_status(user_petition_number_from_db(user_id), user_pin_from_db(user_id))
             last_status_from_db = last_status(user_id)
+            language_code = user_language_from_db(user_id)
             log_record(user_id, fresh_status, 'routine check')
             if fresh_status != last_status_from_db:
                 try:
-                    await bot.send_message(user_id, f'Hey there!!! Here is the new status of your '
-                                                    f'petition: \n '
-                                                    f'{fresh_status}!')
+                    reply_text = (get_translated_message('status_changed_message', language_code)
+                                  % fresh_status)
+                    await bot.send_message(user_id, reply_text)
                 except Exception as error:
-                    raise RuntimeError(f'Message sent error: {error}')
+                    raise RuntimeError('Message sent error: {error}')
                 append_new_status(user_id, fresh_status)
 
 
