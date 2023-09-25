8acb851e94cdb045a575916b3783762a0088a8a2
ekaritskiy <egor.karitskiy@gmail.com>
2023-09-25 09:01:15 +0200

    bug fixes in e-mail operations

--- email_operations.py
+++ email_operations.py
@@ -1,17 +1,18 @@
 import email.utils
 import os
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
 import jinja2
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
 
 load_dotenv()
 
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+SMTP_SERVER = os.getenv('SMTP_SERVER')
+SMTP_PORT = int(os.getenv('SMTP_PORT'))
 
 
 def send_email(to_addr, message, title):
@@ -31,16 +32,15 @@ def send_email(to_addr, message, title):
     msg.attach(MIMEText(message, 'html'))
 
     try:
-        mailserver = smtplib.SMTP('smtp.yandex.ru', 587)
+        mailserver = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
         mailserver.set_debuglevel(False)
         mailserver.ehlo()
         mailserver.starttls()
         mailserver.ehlo()
         mailserver.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        mailserver.sendmail(EMAIL_ADDRESS, 'egor.karitskiy@gmail.com', msg.as_string())
+        mailserver.sendmail(EMAIL_ADDRESS, to_addr, msg.as_string())
         mailserver.quit()
     except smtplib.SMTPException as error:
         raise RuntimeError(
             f"email error: {error}."
         )
-
